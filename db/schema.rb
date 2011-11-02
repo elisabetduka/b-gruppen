d55f34900a118b8a8aca590c918e143d221fa4ec
Hilla <hilla@hilla-1215B-1215B.(none)>
2011-11-02 14:49:16 +0100

    added functionality for checking radiobuttons for category when writing post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111101105207) do
+ActiveRecord::Schema.define(:version => 20111102110008) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -19,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20111101105207) do
     t.boolean  "published"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
 end
