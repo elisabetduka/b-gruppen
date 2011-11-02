7f95f6be09061b6a30588951865317b03f2c1153
Hilla <hilla@hilla-1215B-1215B.(none)>
2011-11-02 11:55:22 +0100

    app so far: posts generated, slightly altered at language and date of post

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BGruppen::Application.config.secret_token = '4636e14bf1d77e6d89b2be39ce2260e555cef093af135b7d2fcd3a08eac04a71aafe2337c74a83612bf03dcb0ad754c6aaf61efd243eaf1c571194ff8a6d161b'
