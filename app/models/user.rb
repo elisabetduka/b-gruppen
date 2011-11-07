63b073ba946497978b0b5edbc1299523e2ca3bc2
Hilla <hilla@hilla-1215B-1215B.(none)>
2011-11-07 22:47:36 +0100

    finished for demo. user, post, category and comment all working.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ActiveRecord::Base
 	has_many :posts
 	has_many :comments
 	
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation
   
   attr_accessor :password
   before_save :encrypt_password
   
-  #validates_confirmation_of :password
+  validates_confirmation_of :password
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
